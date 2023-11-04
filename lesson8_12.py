f86312e3d15da179a673ebbadf64a6b1585c3132
andytsai6g <147892448+andytsai6g@users.noreply.github.com>
2023-11-04 06:25:35 +0000

    modify

---
+++ lesson8_12.py
@@ -0,0 +1,8 @@
+from tools import getStudent
+
+stu1 = getStudent(name="robert",chinese=89,math=92,english=75)
+print(stu1.name)
+print(stu1.chinese)
+print(stu1.english)
+print(stu1.math)
+print(stu1.sum())
